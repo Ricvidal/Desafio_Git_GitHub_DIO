c1967a84df6df81aea30a429cb1ce417294f0d4c
Ricardo Vidal <ricardoqvidal@gmail.com>
2023-03-11 23:51:35 -0300

    Livro sem o toString mostrando endereço na memória

--- eclipseDioGit/src/br/com/dio/AtividadeDioJava.java
+++ eclipseDioGit/src/br/com/dio/AtividadeDioJava.java
@@ -1,12 +1,16 @@
 package br.com.dio;
 
 import br.com.dio.model.Gato;
+import br.com.dio.model.Livro;
 
 public class AtividadeDioJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		Livro livro1 = new Livro("Como aprender Java","","");
+		System.out.println("O endereço da memória onde está o livro é: " + livro1);
 		
 		Gato gato1 = new Gato("Pichano", "pardo", 1);
 		System.out.println(gato1);
